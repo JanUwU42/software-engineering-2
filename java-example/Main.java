682985646041c90bb37f8353ec30aadf1eb119a0
JanUwU42 <coding@jan-osing.simplelogin.com>
2026-06-16 16:30:31 +0200

    Update Main.java

--- java-example/Main.java
+++ java-example/Main.java
@@ -2,63 +2,42 @@ public class Main {
 
     // --- Receiver ---
     static class Light {
-        private final String location;
-
-        Light(String location) { this.location = location; }
-
-        void on()  { System.out.println(location + " light is ON"); }
-        void off() { System.out.println(location + " light is OFF"); }
+        void on()  { System.out.println("Light is ON"); }
+        void off() { System.out.println("Light is OFF"); }
     }
 
     // --- Command Interface ---
-    interface Command {
-        void execute();
-        void undo();
-    }
+    interface Command { void execute(); }
 
     // --- Concrete Commands ---
     static class LightOnCommand implements Command {
         private final Light light;
-
         LightOnCommand(Light light) { this.light = light; }
-
         public void execute() { light.on(); }
-        public void undo()    { light.off(); }
     }
 
     static class LightOffCommand implements Command {
         private final Light light;
-
         LightOffCommand(Light light) { this.light = light; }
-
         public void execute() { light.off(); }
-        public void undo()    { light.on(); }
     }
 
     // --- Invoker ---
     static class RemoteControl {
         private Command command;
-
-        void setCommand(Command command) { this.command = command; }
+        void setCommand(Command c) { command = c; }
         void pressButton() { command.execute(); }
-        void pressUndo()   { command.undo(); }
     }
 
     // --- Demo ---
     public static void main(String[] args) {
-        Light livingRoom = new Light("Living Room");
-
-        Command lightOn  = new LightOnCommand(livingRoom);
-        Command lightOff = new LightOffCommand(livingRoom);
-
+        Light light = new Light();
         RemoteControl remote = new RemoteControl();
 
-        remote.setCommand(lightOn);
-        remote.pressButton();   // Living Room light is ON
+        remote.setCommand(new LightOnCommand(light));
+        remote.pressButton();
 
-        remote.setCommand(lightOff);
-        remote.pressButton();   // Living Room light is OFF
-
-        remote.pressUndo();     // Living Room light is ON
+        remote.setCommand(new LightOffCommand(light));
+        remote.pressButton();
     }
 }
